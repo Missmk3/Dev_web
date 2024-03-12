b328ed2c8425f858dd43e1bf8767cfb5e2a1cb7f
Miss MK <kengnimires003@gmail.com>
2024-03-12 11:24:21 +0100

    connect database 3

---
+++ model/boxes.js
@@ -0,0 +1,12 @@
+class Boxes{
+    id;
+    id_user;
+    id_products;
+    decription;
+    constructor(id,id_user,id_products,decription){
+        this.id = id;
+        this.id_user= id_user;
+        this.id_products = id_products;
+        this.decription = description; 
+    }
+}
